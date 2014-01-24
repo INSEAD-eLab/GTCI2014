f9638855b9d844379749f42ed5c32b3101871fc5
Aung Myint Thein Aug 2012 Key <thein.aungmyint@gmail.com>
2014-01-24 11:31:06 +0800

    added Political stability

--- ISO3.R
+++ ISO3.R
@@ -1,1 +1,7 @@
-mydata <- read.csv("Country List with ISO3.csv", header=TRUE, sep=",")
+install.packages("XLConnect")
+
+library (XLConnect)
+ISO3<-loadWorkbook("Country List with ISO3.xlsx")
+ISO3<-readWorksheet(ISO3, sheet="Country Code", region="B3:C450", header=T)
+
+head(ISO3)
