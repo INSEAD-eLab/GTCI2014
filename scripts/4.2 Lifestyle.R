a06c5a2736305d597d5462cc67e1ae57472eb54f
Aung Myint Thein Aug 2012 Key <thein.aungmyint@gmail.com>
2014-03-07 16:25:22 +0800

    more data

--- scripts/4.2 Lifestyle.R
+++ scripts/4.2 Lifestyle.R
@@ -21,3 +21,15 @@ improved.water.source <- get.UNESCO.format(source.file="[R] [WDI] Improved water
                                            result.cut.year=2003,
                                            names.separated=TRUE, 
                                            country.names="A4:A255")
+
+
+## WDI data has same format as UNESCO and used that function
+################################# Physicians density
+Physicians.density <- get.UNESCO.format(source.file="[R] [WDI] Physicians density.xls",
+                                           source.sheet="Data", 
+                                           source.data.region="AU4:BD255",
+                                           source.colnames="AT3:BD3", 
+                                           result.colnames="Physicians.density",
+                                           result.cut.year=2003,
+                                           names.separated=TRUE, 
+                                           country.names="A4:A255")
