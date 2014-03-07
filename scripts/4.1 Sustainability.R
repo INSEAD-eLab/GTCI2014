db0b5694cb64cd21d9bad9a9cee785528c843c1a
Aung Myint Thein Aug 2012 Key <thein.aungmyint@gmail.com>
2014-03-07 15:54:55 +0800

    added more data

--- scripts/4.1 Sustainability.R
+++ scripts/4.1 Sustainability.R
@@ -14,3 +14,15 @@ Extent.effect.taxation.on.incentives.to.invest <- get.WEF(source.file="WEF.xlsx"
                                                         source.colname="CP6", 
                                                         source.date="C7", 
                                                         source.countries="C8:C155")
+
+
+## WDI data has same format as UNESCO and used that function
+################################# Labour tax and contributions
+Labour.tax.and.contributions <- get.UNESCO.format(source.file="[R] [WDI] Labour tax and contributions.xls",
+                                           source.sheet="Data", 
+                                           source.data.region="BD4:BE255",
+                                           source.colnames="BC3:BE3", 
+                                           result.colnames="Labour.tax.and.contributions",
+                                           result.cut.year=2003,
+                                           names.separated=TRUE, 
+                                           country.names="A4:A255")
