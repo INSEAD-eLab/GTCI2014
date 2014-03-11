4f150a0789514f244e19000ae01ce64cb512eec8
Aung New PC from INSEAD <myintthein.aung@insead.edu>
2014-03-11 15:06:53 +0800

    added data for 5.1 data

---
+++ scripts/5.1 EmployableSkills.R
@@ -0,0 +1,83 @@
+################# Technicians and associate professionals
+##
+## Possible colnames for tech and associates (68, 88, 08)
+##[1] "Country..code."               "Country"                      "B"                            "Year"                         "Sex..code."                  
+##[6] "Sex"                          "Total.employment...000."      "X0...000."                    "X0...."                       "X1...000."                   
+##[11] "X1...."                       "X2...000."                    "X2...."                       "X3...000."                    "X3...."                      
+##[16] "X4...000."                    "X4...."                       "X5...000."                    "X5...."                       "X6...000."                   
+##[21] "X6...."                       "X7...000."                    "X7...."                       "X8...000."                    "X8...."                      
+##[26] "X9...000."                    "X9...."                       "X...000."                     "X...."                        "Repository..code."           
+##[31] "Repository"                   "Type.of.source..code."        "Type.of.source"               "Coverage..code."              "Coverage"                    
+##[36] "Coverage..employment...code." "Coverage..employment."        "Age"                          "Reference.period"             "Geographic.limitation"       
+##[41] "Coverage.limitation"          "Survey.limitation"            "Classification.remark"        "Notes"     
+
+
+tech.asso.68.MF.latest <- get.ILO.latest( source.file="[R] [ILO] [ISCO-68] Technicians and associate professionals.xls",
+                                          source.sheet="KILM 5c",
+                                          source.region="A3:AN1976", 
+                                          source.gender="MF",
+                                          source.colnames=c("Country.Name", "ISO3", "Year", "Sex", "Total.employment...000.",
+                                                            "X0.1...000.", "X0.1....", "X2...000.", "X2...."),
+                                          result.colnames=c("Country.Name", "ISO3", "Year", "Sex", "Total.employment.000.68",
+                                                            "01.000.68", "01.percent.68", "2.000.68", "2.percent.68"),
+                                          result.cut.year=2003)
+
+
+tech.asso.88.MF.latest <- get.ILO.latest(source.file="[R] [ILO] [ISCO-88] Technicians and associate professionals.xls",
+                                         source.sheet="KILM 5b", 
+                                         source.region="A3:AR3888", 
+                                         source.gender="MF",
+                                         source.colnames=c("Country.Name", "ISO3", "Year", "Sex", "Total.employment...000.",
+                                                           "X3...000.", "X3...."),
+                                         result.colnames=c("Country.Name", "ISO3", "Year", "Sex", "Total.employment.000.88",
+                                                           "3.000.88", "3.percent.88"),
+                                         result.cut.year=2003)
+
+
+tech.asso.08.MF.latest <- get.ILO.latest(source.file="[R] [ILO] [ISCO-08] Technicians and associate professionals.xls",
+                                         source.sheet="KILM 5a",
+                                         source.region="A3:AR217", 
+                                         source.gender="MF",
+                                         source.colnames=c("Country.Name", "ISO3", "Year", "Sex", "Total.employment...000.",
+                                                           "X3...000.", "X3...."),
+                                         result.colnames=c("Country.Name", "ISO3", "Year", "Sex", "Total.employment.000.08",
+                                                           "3.000.08", "3.percent.08"), 
+                                         result.cut.year=2003)
+
+
+## to find out which country is only available in 68 but not in 88 and 08
+setdiff(tech.asso.68.MF.latest$Country.Name, union(tech.asso.88.MF.latest$Country.Name, tech.asso.08.MF.latest$Country.Name))
+
+############# for female data
+
+tech.asso.68.F.latest <- get.ILO.latest( source.file="[R] [ILO] [ISCO-68] Technicians and associate professionals.xls",
+                                          source.sheet="KILM 5c",
+                                          source.region="A3:AN1976", 
+                                          source.gender="F",
+                                          source.colnames=c("Country.Name", "ISO3", "Year", "Sex", "Total.employment...000.",
+                                                            "X0.1...000.", "X0.1....", "X2...000.", "X2...."),
+                                          result.colnames=c("Country.Name", "ISO3", "Year", "Sex", "Total.employment.000.68",
+                                                            "01.000.68", "01.percent.68", "2.000.68", "2.percent.68"),
+                                          result.cut.year=2003)
+
+
+tech.asso.88.F.latest <- get.ILO.latest(source.file="[R] [ILO] [ISCO-88] Technicians and associate professionals.xls",
+                                         source.sheet="KILM 5b", 
+                                         source.region="A3:AR3888", 
+                                         source.gender="F",
+                                         source.colnames=c("Country.Name", "ISO3", "Year", "Sex", "Total.employment...000.",
+                                                           "X3...000.", "X3...."),
+                                         result.colnames=c("Country.Name", "ISO3", "Year", "Sex", "Total.employment.000.88",
+                                                           "3.000.88", "3.percent.88"),
+                                         result.cut.year=2003)
+
+
+tech.asso.08.F.latest <- get.ILO.latest(source.file="[R] [ILO] [ISCO-08] Technicians and associate professionals.xls",
+                                         source.sheet="KILM 5a",
+                                         source.region="A3:AR217", 
+                                         source.gender="F",
+                                         source.colnames=c("Country.Name", "ISO3", "Year", "Sex", "Total.employment...000.",
+                                                           "X3...000.", "X3...."),
+                                         result.colnames=c("Country.Name", "ISO3", "Year", "Sex", "Total.employment.000.08",
+                                                           "3.000.08", "3.percent.08"), 
+                                         result.cut.year=2003)
