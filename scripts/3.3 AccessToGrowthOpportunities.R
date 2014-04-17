c40baa712c0174e35b5f5a069948274e4366dcdc
Aung New PC from INSEAD <myintthein.aung@insead.edu>
2014-04-17 13:49:55 +0800

    added LinkedIn Data

--- scripts/3.3 AccessToGrowthOpportunities.R
+++ scripts/3.3 AccessToGrowthOpportunities.R
@@ -99,28 +99,13 @@ citable.documents.H.index  <- get.WEF(source.file="[R] [SCImago] Citable documen
                                       source.countries="B2:B239")
 
 
-################# LinkedIn users
-linkedIn.users  <- get.WEF(source.file="[R] Linkedin users (numerator).xlsx", 
-                                      source.sheet="Sheet1", 
-                                      source.data.region="B4:B97",
-                                      source.colname="B3", 
-                                      source.date="A1", 
-                                      source.countries="A4:A97")
 
-labor.force <- get.ILO.latest( source.file="labour force.csv",
-                               source.sheet="KILM 1a",
-                               source.region="A3:G104331", 
-                               source.gender="MF",
-                               source.colnames=c("Country.Name", "ISO3", "Year", "Sex", "Age.group", "Labour.force...000."),
-                               result.colnames=c("Country.Name", "ISO3", "Year", "Sex", "Age.group", "Labour.force.000"),
-                               source.age="15-64",
-                               result.cut.year=2003,
-                               data.format="csv")
+## scripts to run codes for linked in new
+## object name : linkedIn.users
+## colnames : linkedin.ratio
+source("scripts/linkedInUsers.R")
+  
 
-linkedIn.users.ratio <- merge(linkedIn.users, labor.force, by="ISO3", all.x=T, sort=F)
-linkedIn.users.ratio[, 9] <- lapply(linkedIn.users.ratio[9], function(x) ifelse(is.na(x), NA, as.numeric(gsub(" ", "", as.character(x)))))
-
-linkedIn.users.ratio[, "ratio"] <- linkedIn.users.ratio[, 3]/(linkedIn.users.ratio[, 9]*1000)
 
 
 
