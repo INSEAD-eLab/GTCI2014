a299fc2a079f538ccb170fb99b15ef396cac4267
Aung New PC from INSEAD <myintthein.aung@insead.edu>
2014-06-26 15:43:49 +0800

    cleaned the testing codes

--- scripts/5.1 EmployableSkills.R
+++ scripts/5.1 EmployableSkills.R
@@ -218,5 +218,3 @@ tertiary.educated.population.UNESCO  <- get.WB.format(source.file="[R] [UNESCO] 
 
 secondary.educated.population.UNESCO <- merge(secondary.educated.population.UNESCO, tertiary.educated.population.UNESCO[,-1], by=c("ISO3", "Year"), all=TRUE)
 secondary.educated.population.UNESCO[, "only secondary educated population"] <- secondary.educated.population.UNESCO[, "secondary educated population"] - secondary.educated.population.UNESCO[, "tertiary educated population"]
-
-## start
